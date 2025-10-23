bbc8325e547e12fd017c8ab1c0e600939a13e392
gadimahendra <gadi.mahendra@outlook.com>
2025-10-23 15:53:07 +0200

    Bugs fixed

--- src/app/components/dashboard-statistics/dashboard-statistics.component.ts
+++ src/app/components/dashboard-statistics/dashboard-statistics.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { CardModule } from 'primeng/card';
-import { EnrolmentState } from '../../../store/states/enrollment.state';
 import { getDashboardData } from '../../../store/actions/enrollment.action';
 import { IdashboardStats } from '../../../core/model/enrollment.model';
 import { DialogModule } from 'primeng/dialog';
 import { TableModule } from 'primeng/table';
+import { MessageService } from 'primeng/api';
 // import { ChartModule } from 'primeng/chart';
 
 @Component({
@@ -15,35 +15,24 @@ import { TableModule } from 'primeng/table';
   styleUrl: './dashboard-statistics.component.scss',
 })
 export class DashboardStatisticsComponent implements OnInit {
-  totalStudents = 1248;
-  totalDepartments = 12;
-  fullyPaid = 820;
-  partiallyPaid = 428;
-
   displayDialog: boolean = false;
-
   dashboardStatics!: IdashboardStats;
-
-  paymentChartData: any;
-  paymentChartOptions: any;
   private store = inject(Store);
+  private messageService = inject(MessageService);
+
   ngOnInit() {
     this.store.dispatch(new getDashboardData()).subscribe({
       next: (res: any) => {
-        console.log('response', res);
         this.dashboardStatics = res.enrollment.DashboardStats;
       },
       error: (err) => {
         console.log('error', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Failed to Fetch Dashboard details. Please try again.',
+        });
       },
     });
-  }
-
-  openDialog() {
-    this.displayDialog = true;
-  }
-
-  closeDialog() {
-    this.displayDialog = false;
   }
 }
